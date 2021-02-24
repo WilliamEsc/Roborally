61f621150940ea790d6a6d576c41130f246e5d4a
p1706188 <malaurie.gomez@etu.univ-lyon1.fr>
2021-02-24 17:38:45 +0100

    board

--- Src/board.hpp
+++ Src/board.hpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <unordered_map>
 #include <bitset>
+#include <vector>
 
 namespace RR {
 
@@ -141,6 +142,58 @@ struct Board {
   /* the tiles are stored in a hash table */
 
   std::unordered_map<Location, TileType, LocationHash> tiles ;
+
+} ;
+
+class Cellule {
+public:
+  std::vector<Cellule*> tab;
+  Robot::Status stat;
+  Location loc;
+
+  Cellule(Robot::Status s, Location l);
+
+  ~Cellule();
+} ;
+
+class Graph
+{
+  public :
+
+    /* construction d'une liste vide */
+    Graph(unsigned int x_, unsigned int y_) ;
+
+    /* construction par copie */
+    Graph(const Graph& autre) ;
+
+    /* affectation */
+    Graph& operator=(const Graph& autre) ;
+
+    /* destruction */
+    ~Graph() ;
+
+    /* consultation de la tete */
+    const Cellule* tete() const ;
+    Cellule* tete() ;
+
+    /* insert l'élément dans la liste */
+    void insert(int v);
+
+    /* taille de la liste */
+    int taille() const ;
+
+    /* recherche dans la liste */
+    Cellule* recherche(int v) ;
+
+    /* affichage */
+    void afficher() const ;
+
+  private :
+
+    Cellule* first;
+    unsigned int x, y;
+
+
 } ;
 
 } //end of namespace RR
